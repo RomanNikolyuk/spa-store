6a45df4a64aca3a7fc2c65ba82a80c8e3cb476c9
vl_st_ <roman.nikolyuk@gmail.com>
2022-03-17 21:34:57 +0200

    Видалено папку Components

--- app/Http/Controllers/Api/ProductsController.php
+++ app/Http/Controllers/Api/ProductsController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Components\CollectionPaginator;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductsResource;
 use App\Models\Category;
